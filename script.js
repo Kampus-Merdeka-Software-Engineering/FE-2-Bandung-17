89d4827c1244bda7af9b2367fc81409eb935419a
Husain2505 <haidarhusain25@gmail.com>
2023-11-27 13:03:41 +0700

    create shopping-cart, & update humberger menu  dll

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// toogle kelas active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
 // slider baranda
 let slideIndex = 1;
 showSlides(slideIndex);

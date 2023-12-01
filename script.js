3bf8463a4d4ca80d25af5dd42cd3062608ed5216
sovianajanud <148770192+sovianajanud@users.noreply.github.com>
2023-12-01 20:28:56 +0700

    update popup detail, keranjang, tentang kami
    update pop up detail menu minuman dan makanan
    update tampilan html dan css keranjang
    update Galeri Group 17 tentang kami

--- script.js
+++ script.js
@@ -66,59 +66,33 @@ document.getElementById("beranda").addEventListener("click", function() {
   this.style.fontWeight = "bold";
 });
 
-var itemCount = 1; // Default item count
+// var itemCount = 1; // Default item count
 
-  function decrementItem() {
-    if (itemCount > 1) {
-      itemCount--;
-      updateItemCount();
-    }
-  }
+//   function decrementItem() {
+//     if (itemCount > 1) {
+//       itemCount--;
+//       updateItemCount();
+//     }
+//   }
 
-  function incrementItem() {
-    itemCount++;
-    updateItemCount();
-  }
+//   function incrementItem() {
+//     itemCount++;
+//     updateItemCount();
+//   }
 
- function removeItem() {
-    itemCount = 0;
-    updateItemCount();
-  }
+//  function removeItem() {
+//     itemCount = 0;
+//     updateItemCount();
+//   }
 
-  function updateItemCount() {
-    if (!isNaN(itemCount)) {
-      document.getElementById("itemCount").textContent = itemCount;
-    } else {
-      itemCount = 0;
-      document.getElementById("itemCount").textContent = itemCount;
-    }
-   }
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+//   function updateItemCount() {
+//     if (!isNaN(itemCount)) {
+//       document.getElementById("itemCount").textContent = itemCount;
+//     } else {
+//       itemCount = 0;
+//       document.getElementById("itemCount").textContent = itemCount;
+//     }
+//    }
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
@@ -127,7 +101,7 @@ function myFunction() {
 }
 
 function filterFunction() {
-  var input, filter, ul, li, a, i;
+    var input, filter, ul, li, a, i;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   div = document.getElementById("myDropdown");
@@ -139,5 +113,123 @@ function filterFunction() {
     } else {
       a[i].style.display = "none";
     }
+  }
+}
+
+// Keranjang
+if (document.readyState == 'loading'){
+  document.addEventListener('DOMContentLoaded', ready);
+} else {
+  ready();
+}
+
+// Making function
+function ready(){
+  // Remove item From Cart
+  var removeCartButtons = document.getElementsByClassName('cart-remove')
+  console.log(removeCartButtons)
+  for(var i = 0; i < removeCartButtons.length; i++){
+    var button = removeCartButtons[i]
+    button.addEventListener("click", removeCartItem)
+  }
+  
+  // Quantity Changes
+  var quantityInputs = document.getElementsByClassName('cart-quantity')
+  for (var i=0; i < quantityInputs.length; i++){
+    var input = quantityInputs[i];
+    input.addEventListener('change', quantityChanged);
+  }
+  // Add to Cart
+  var addCart =document.getElementsById('shopping-cart-button')
+  for (var i=0; i < addCart.length; i++){
+    var button = addCart[i];
+    input.addEventListener('click', addCartClicked);
+  }
+  alert('hll')
+}
+
+// Remove item From Cart
+function removeCartItem(event){
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+}
+
+// Quantity Changes
+function quantityChanged(event){
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0){
+    input.value = 1;
+  }
+  updatetotal();
+}
+
+// Add to Cart
+function addCartClicked(event){
+  var buttonCartClicked = event.target;
+  var shopProducts = buttonCartClicked.parentElement
+  var title = shopProducts.getElementsByClassName('product-title').innerText
+  var price = shopProducts.getElementsByClassName('price')[0].innerText
+  var productImg = shopProducts.getElementsByClassName('product-img')[0].scr
+  addProductToCart(title, price, productImg);
+  updatetotal();
+}
+
+function addProductToCart(title, price, productImg){
+  var cartShopBox = document.createElement('div')
+  // cartShopBox.classList.add('trash-icon')
+  var cartItems = document.getElementsByClassName('cart')[0]
+  var cartItemsNames = cartItems.getElementsByClassName('cart-product-title')
+  for (var i = 0; i < cartItemsNames.length; i++){
+    alert('You have already add this item to cart')
+  }
+
+}
+
+// Update total
+function updatetotal(){
+  var cart = document.getElementsByClassName('cart')[0];
+  var cartBoxes = cart.getElementsByClassName('trash-icon')[0];
+  var total = 0;
+  for (var i = 0; i < cartBoxes.length; i++){
+    var cartBox = cartBoxes[i]
+    var priceElemen = cartBox.getElementsByClassName('cart-price')
+    var quantityELement = cartBox.getElementsByClassName('cart-quantity') 
+    var price = priceElemen.innerText.replace("Rp", "")
+    var quantity = quantityELement.value
+    total = total + (price * quantity)
+
+    // if price contain some cents value
+
+    document.getElementsByClassName('total-price')[0].innerText = "Rp" + total;
+  }
+}
+
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+console.log('coba masuk sana')
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+  console.log('masuk sini')
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 }

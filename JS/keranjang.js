f1295802db85f60987ff63c3e57711d39727e0bb
Husain2505 <haidarhusain25@gmail.com>
2023-12-04 20:50:00 +0700

    change name home.html to index.html

--- JS/keranjang.js
+++ JS/keranjang.js
@@ -38,18 +38,40 @@ function removeCartItem(event, cart) {
   var buttonClicked = event.target;
   cart.remove();
 }
+// set value in localstorage
+function setStorage() {
+  localStorage.setItem("produk-item", JSON.stringify(items));
+}
 
 // adding data to localstorage
 const attTocartBtn = document.getElementsByClassName("attToCart");
 let items = [];
 for (let i = 0; i < attTocartBtn.length; i++) {
   attTocartBtn[i].addEventListener("click", function (e) {
-    console.log(e.target.parentElement.children[0]);
     if (typeof Storage !== "undefined") {
       let item = {
         id: i + 1,
-        name: e.target.parentElement.children[0],
+        name: e.target.parentElement.children[0].textContent,
+        price: e.target.parentElement.children[1].children[0].textContent,
+        no: 1,
       };
+      if ((JSON, parse(localStorage.getItem("items")) === null)) {
+        items.push(item);
+        localStorage.setItem("items", JSON.stringify(items));
+        window.location.reload();
+      } else {
+        const localItems = JSON.parse(localStorage.getItem("items"));
+        localItems.map((data) => {
+          if (item.id == data.id) {
+            item.no = data.no + 1;
+          } else {
+            items.push(data);
+          }
+        });
+        items.push(item);
+        localStorage.setItem("items", JSON.stringify(items));
+        window.location.reload();
+      }
     } else {
       alert("local storage is not working on your browser");
     }

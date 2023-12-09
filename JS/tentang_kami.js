aeee03f81e61938c72187155b8957f2badd8a9c1
sovianajanud <148770192+sovianajanud@users.noreply.github.com>
2023-12-10 00:46:32 +0700

    Update tentang_kami.js

--- JS/tentang_kami.js
+++ JS/tentang_kami.js
@@ -1,24 +1,68 @@
-// method POST kontak kami
-function setupContactForm() {
-    const form = document.getElementById("formPesan");
-    console.log(form, "ini form");
-    form.addEventListener("submit", async function (event) {
-      event.preventDefault();
-      const formData = new FormData(form);
-      const formProps = Object.fromEntries(formData);
-      console.log(formProps, "ini form props");
-      // try {
-      //   const response = await fetch(`${apiUrl}/contact`, {
-      //     method: "POST",
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //     },
-      //     body: JSON.stringify(formProps),
-      //   });
-      //   const data = await response.json();
-      //   console.log("Success:", data);
-      // } catch (error) {
-      //   console.error("Error:" error);
-      // }
+document.addEventListener("DOMContentLoaded", function () {
+  const messageForm = document.getElementById("formPesan");
+ 
+  messageForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+ 
+    const name = document.getElementById("name").value;
+    const address = document.getElementById("address").value;
+    const message = document.getElementById("message").value;
+ 
+    if (!name || !address || !message) {
+      showSweetAlert(
+        "Error",
+        "Silakan lengkapi semua kolom formulir!",
+        "error"
+      );
+      return;
+    }
+ 
+    const formData = {
+      name,
+      address,
+      message,
+    };
+ 
+    // Kirim data ke backend
+    fetch("https://be-2-bandung-17-production.up.railway.app/message", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Terjadi kesalahan saat mengirim formulir.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        showSweetAlert(
+          "Success",
+          "Pesan Anda berhasil dikirim! TERBAIK",
+          "success"
+        );
+      })
+      .catch((error) => {
+        console.error("Error:", error.message);
+        showSweetAlert(
+          "Error",
+          "Terjadi kesalahan saat mengirim Pesan. Silakan coba lagi nanti.",
+          "error"
+        );
+      });
+  });
+ 
+  function showSweetAlert(title, text, icon) {
+    // Menampilkan SweetAlert
+    Swal.fire({
+      title: title,
+      text: text,
+      icon: icon,
+      confirmButtonColor: "#645cff",
     });
   }
+});
+
+
